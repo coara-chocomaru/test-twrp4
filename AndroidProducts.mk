c2b8f0f1bb878e98223a06620fdf6727e82e206b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-03 09:16:35 +0900

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_certus.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_certus-user \
+    omni_certus-userdebug \
+    omni_certus-eng
