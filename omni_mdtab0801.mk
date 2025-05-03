433f14e323e3c986fb127594e15131321805cd61
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-03 09:26:50 +0900

    Create omni_mdtab0801.mk

---
+++ omni_mdtab0801.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_DEVICE := mdtab0801
+PRODUCT_NAME := omni_mdtab0801
+PRODUCT_BRAND := h2
+PRODUCT_MODEL := SMAO
+PRODUCT_MANUFACTURER := h2
