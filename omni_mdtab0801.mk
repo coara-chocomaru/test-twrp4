2745fb8b16e5d9d03db06e1730151d94d8c34200
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-04 15:38:46 +0900

    Update omni_mdtab0801.mk

--- omni_mdtab0801.mk
+++ omni_mdtab0801.mk
@@ -3,13 +3,7 @@
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := mdtab0801
 PRODUCT_NAME := omni_mdtab0801
