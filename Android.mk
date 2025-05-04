0ff163dc8eaeae0aa0bca69f67e82728651d78e4
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-04 10:43:48 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -5,8 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
 LOCAL_PATH := $(call my-dir)
-LOCAL_MULTILIB := 32
 ifeq ($(TARGET_DEVICE),mdtab0801)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
