1603f204772cd2428c837711bb6e1a811976dc18
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-05 18:37:51 +0900

    Update omni_mdtab0801.mk

--- omni_mdtab0801.mk
+++ omni_mdtab0801.mk
@@ -6,10 +6,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, device/h2/mdtab0801/device.mk)
+$(call inherit-product, device/H2/mdtab0801/device.mk)
 
 PRODUCT_DEVICE := mdtab0801
 PRODUCT_NAME := omni_mdtab0801
-PRODUCT_BRAND := h2
+PRODUCT_BRAND := H2
 PRODUCT_MODEL := SMAO
-PRODUCT_MANUFACTURER := h2
+PRODUCT_MANUFACTURER := alps
+
+PRODUCT_GMS_CLIENTID_BASE := android-h2
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="MD-TAB0801-user 9 01.00.000 01.00.000 test-keys"
+
+BUILD_FINGERPRINT := alps/full_k62v1_32_bsp/k62v1_32_bsp:9/PPR1.180610.011/lm08101556:user/test-keys
