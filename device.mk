9c67e96e4694784412ddd412155c32bec58551da
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-08-12 13:45:15 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,26 @@
 #
 
 LOCAL_PATH := device/h2/mdtab0801
+
+# API levels
+PRODUCT_TARGET_VNDK_VERSION := 28
+PRODUCT_SHIPPING_API_LEVEL := 28
+
+# Product characteristics
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+　　 update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-impl.recovery \
+    android.hardware.health@2.0-service
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
