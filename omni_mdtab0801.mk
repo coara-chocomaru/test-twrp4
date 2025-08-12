2f0cad55e06d51d899d79534f27921d2edced680
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-08-12 14:41:06 +0900

    Update omni_mdtab0801.mk

--- omni_mdtab0801.mk
+++ omni_mdtab0801.mk
@@ -4,6 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/h2/mdtab0801/device.mk)
 
