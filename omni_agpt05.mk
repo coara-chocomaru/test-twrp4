17d7f446fcb606c7e30a7a7c37022e9a75e2ab9e
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-09-10 21:52:12 +0900

    Update omni_agpt05.mk

--- omni_agpt05.mk
+++ omni_agpt05.mk
@@ -3,8 +3,10 @@
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/zechin/agpt05/device.mk)
 
@@ -15,6 +17,4 @@ PRODUCT_MODEL := AGP-T05
 PRODUCT_MANUFACTURER := zechin
 PRODUCT_DEFAULT_LANGUAGE := en
 PRODUCT_DEFAULT_REGION   := US
-#PRODUCT_AAPT_CONFIG := normal
-#PRODUCT_AAPT_PREF_CONFIG := mdpi
 PRODUCT_GMS_CLIENTID_BASE := android-zechin
