9bcfc4e5a74735d6ff96e6a7cc95e404b5b6e7c5
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-06-07 03:48:41 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,10 @@
 #
 
 LOCAL_PATH := device/zechin/agpt05
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.product.model=AGP-T05 \
+    ro.product.brand=AGP \
+    ro.product.name=AGP-T05 \
+    ro.product.phone.model=AGP-T05 \
+    ro.lxt.custom.model=AGP-T05
